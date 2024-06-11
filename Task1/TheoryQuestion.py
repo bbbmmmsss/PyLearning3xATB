b39f34b61e0734a833b1352250e08ebf3e91312c
bhagyashree mahesh sawant <bhagyashreesawant19@gmail.com>
2024-06-11 23:50:33 +0530

    Task Question

---
+++ Task1/TheoryQuestion.py
@@ -0,0 +1,22 @@
+# 1. Explain the difference between the = operator and the == operator in Python.
+# = is used to assign a value to a variable.
+# while == is used to compare two values.
+# example:
+t=5
+u=5
+t==u
+print(t==u)
+
+# 2. What does the ** operator do in Python, and how is it used?
+# ** is used to calculate power of a number.
+# example:
+r=10
+s=40
+print(r**s)
+
+# What does the ^ operator do in Python, and in what context is it commonly used?
+# ^ is used to calculate xor of two numbers.
+# example:
+a=10
+b=40
+print(a^b)
---
+++ Task2/Print First Number is Greater Or Not.py
@@ -0,0 +1,10 @@
+# Develop a Python script that calculates the square and cube of a given number. num = 2 sq - 4, c = 8
+
+First_Number = int(input("Enter the First Number: "))
+Second_Number = int(input("Enter the Second Number: "))
+if First_Number > Second_Number:
+    print("First Number is Greater")
+elif First_Number < Second_Number:
+    print("Second Number is Greater")
+else:
+    print("First Number equal to Second Number")
